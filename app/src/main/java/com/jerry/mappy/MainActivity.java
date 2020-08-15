799eac3b57136e444c938bc6b438a8f4eb8814b9
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-08-15 16:30:01 +0530

    complete code

--- app/src/main/java/com/jerry/mappy/MainActivity.java
+++ app/src/main/java/com/jerry/mappy/MainActivity.java
@@ -2,7 +2,16 @@ package com.jerry.mappy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isServiceOK()){
+            init();
+        }
+    }
+
+    public void init(){
+
+        Button btnMap = findViewById(R.id.btn);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public boolean isServiceOK(){
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,9001);
+            dialog.show();
+        }else{
+            Toast.makeText(this,"you cant make map request",Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/jerry/mappy/PlaceAutocompleteAdapter.java
@@ -0,0 +1,173 @@
+package com.jerry.mappy;
+
+
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.data.DataBufferUtils;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.text.style.CharacterStyle;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+public class PlaceAutocompleteAdapter extends ArrayAdapter<AutocompletePrediction> implements Filterable {
+
+    private static final String TAG = "PlaceAutoCompleteAd";
+    private static final CharacterStyle STYLE_BOLD = new StyleSpan(Typeface.BOLD);
+    private ArrayList<AutocompletePrediction> mResultList;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LatLngBounds mBounds;
+
+    private AutocompleteFilter mPlaceFilter;
+
+    public PlaceAutocompleteAdapter(Context context, GoogleApiClient googleApiClient,
+                                    LatLngBounds bounds, AutocompleteFilter filter) {
+        super(context, android.R.layout.simple_expandable_list_item_2, android.R.id.text1);
+        mGoogleApiClient = googleApiClient;
+        mBounds = bounds;
+        mPlaceFilter = filter;
+    }
+
+    public void setBounds(LatLngBounds bounds) {
+        mBounds = bounds;
+    }
+
+    @Override
+    public int getCount() {
+        return mResultList.size();
+    }
+    @Override
+    public AutocompletePrediction getItem(int position) {
+        return mResultList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = super.getView(position, convertView, parent);
+
+        // Sets the primary and secondary text for a row.
+        // Note that getPrimaryText() and getSecondaryText() return a CharSequence that may contain
+        // styling based on the given CharacterStyle.
+
+        AutocompletePrediction item = getItem(position);
+
+        TextView textView1 = (TextView) row.findViewById(android.R.id.text1);
+        TextView textView2 = (TextView) row.findViewById(android.R.id.text2);
+        textView1.setText(item.getPrimaryText(STYLE_BOLD));
+        textView2.setText(item.getSecondaryText(STYLE_BOLD));
+
+        return row;
+    }
+
+    /**
+     * Returns the filter for the current set of autocomplete results.
+     */
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+
+                // We need a separate list to store the results, since
+                // this is run asynchronously.
+                ArrayList<AutocompletePrediction> filterData = new ArrayList<>();
+
+                // Skip the autocomplete query if no constraints are given.
+                if (constraint != null) {
+                    // Query the autocomplete API for the (constraint) search string.
+                    filterData = getAutocomplete(constraint);
+                }
+
+                results.values = filterData;
+                if (filterData != null) {
+                    results.count = filterData.size();
+                } else {
+                    results.count = 0;
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                if (results != null && results.count > 0) {
+                    // The API returned at least one result, update the data.
+                    mResultList = (ArrayList<AutocompletePrediction>) results.values;
+                    notifyDataSetChanged();
+                } else {
+                    // The API did not return any results, invalidate the data set.
+                    notifyDataSetInvalidated();
+                }
+            }
+
+            @Override
+            public CharSequence convertResultToString(Object resultValue) {
+                // Override this method to display a readable result in the AutocompleteTextView
+                // when clicked.
+                if (resultValue instanceof AutocompletePrediction) {
+                    return ((AutocompletePrediction) resultValue).getFullText(null);
+                } else {
+                    return super.convertResultToString(resultValue);
+                }
+            }
+        };
+    }
+
+    private ArrayList<AutocompletePrediction> getAutocomplete(CharSequence constraint) {
+        if (mGoogleApiClient.isConnected()) {
+            Log.i(TAG, "Starting autocomplete query for: " + constraint);
+
+            // Submit the query to the autocomplete API and retrieve a PendingResult that will
+            // contain the results when the query completes.
+            PendingResult<AutocompletePredictionBuffer> results =
+                    Places.GeoDataApi
+                            .getAutocompletePredictions(mGoogleApiClient, constraint.toString(),
+                                    mBounds, mPlaceFilter);
+
+            AutocompletePredictionBuffer autocompletePredictions = results
+                    .await(60, TimeUnit.SECONDS);
+
+            final Status status = autocompletePredictions.getStatus();
+            if (!status.isSuccess()) {
+                Toast.makeText(getContext(), "Error contacting API: " + status.toString(),
+                        Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "Error getting autocomplete prediction API call: " + status.toString());
+                autocompletePredictions.release();
+                return null;
+            }
+
+            Log.i(TAG, "Query completed. Received " + autocompletePredictions.getCount()
+                    + " predictions.");
+
+            // Freeze the results immutable representation that can be stored safely.
+            return DataBufferUtils.freezeAndClose(autocompletePredictions);
+        }
+        Log.e(TAG, "Google API client is not connected for autocomplete query.");
+        return null;
+    }
+
+
+}
